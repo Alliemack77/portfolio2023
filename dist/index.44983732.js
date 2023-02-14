1cf9c38f18549da6d9e9422284eb2f2133099b4c
Allison Leggett <alliemack77@yahoo.com>
2023-02-13 18:57:33 -0800

    initial commit

---
+++ dist/index.44983732.js
@@ -0,0 +1,7 @@
+const navContainer = document.getElementById("navigation-container");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+navToggle.addEventListener("click", ()=>{
+    console.log("Clicked Here");
+});
+
+//# sourceMappingURL=index.44983732.js.map
---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+
+const navContainer = document.getElementById('navigation-container')
+const navToggle = document.querySelector('.mobile-nav-toggle')
+const navLinks = document.querySelectorAll('.nav-link')
+
+navToggle.addEventListener('click', () => {
+    navContainer.classList.toggle('nav-open')
+})
+
+
+
+
+
